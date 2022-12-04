4bf45cd85ac7dcd5e8477f88e0c7d03728200b89
Gourang-pro <117590608+Gourang-pro@users.noreply.github.com>
2022-12-04 23:17:14 +0530

    Add files via upload

---
+++ loop.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+    int n,i,j;
+printf("enter the number :");
+scanf("%d",&n);
+             
+        for(j=1;j<=n;j++)
+            { 
+                printf("%d  ",n);
+
+            }
+               printf("\n");
+                
+return 0;
+
+}
